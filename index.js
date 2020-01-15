96a694786815dd7b4f2616be2312133960bdb94e
alanMarcosA <alanMarcosA@github.com>
2020-01-15 14:38:36 -0300

    funcionalidad a los botones

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ cliente.connect(function(err, client) {
   baseDeDatos = client.db(dbName);
 });
 
-servidor.use(bodyParser.json())
+servidor.use(bodyParser.json());
 
 servidor.listen(port, function() {
   console.log(`escuchando conexiones en ${port}`);
@@ -40,25 +40,38 @@ servidor.get("/css/estilos.css", function(consulta, respuesta) {
   respuesta.sendFile(path.join(__dirname, "publico/css", "estilos.css"));
 });
 
-servidor.post("/guardar", async (consulta, respuesta)=>{
-    var data = consulta.body
-    await agregarNota(data.nota)
-    respuesta.json({res:'nota agregada'})
-})
-function agregarNota(titulo, nota){
-    return baseDeDatos.collection('notas').insertOne({titulo:titulo, nota:nota})
+servidor.get("/script/javasScript.js", function(consulta, respuesta) {
+  respuesta.sendFile(path.join(__dirname, "publico/script", "javasScript.js"));
+});
+
+servidor.post("/guardar", async (consulta, respuesta) => {
+  var data = consulta.body;
+  await agregarNota(data.titulo, data.nota);
+  respuesta.json({ res: "nota agregada" });
+});
+function agregarNota(titulo, nota) {
+  return baseDeDatos
+    .collection("notas")
+    .insertOne({ titulo: titulo, nota: nota });
 }
 
-servidor.get("/getNotas", async (consulta, respuesta) =>{
-    var datos = await baseDeDatos.collection('notas').find().toArray()
-    respuesta.json(datos)
-})
-servidor.delete("/delete", async function(consulta, respuesta){
-    var data = consulta.body
-    baseDeDatos.collection('notas').deleteOne({titulo:data.titulo})
-})
-servidor.patch("./edit", async (consulta, repuesta) =>{
-    var data = consulta.body
-    let nota = data.nota
-    baseDeDatos.collection('notas').updateOne({ titulo: data.titulo }, { $set: {nota}});
-})
+servidor.get("/getNotas", async (consulta, respuesta) => {
+  var datos = await baseDeDatos
+    .collection("notas")
+    .find()
+    .toArray();
+  respuesta.json(datos);
+});
+servidor.delete("/delete", async function(consulta, respuesta) {
+  var data = consulta.body;
+  await baseDeDatos.collection("notas").deleteOne({ titulo: data.titulo });
+  respuesta.json({ res: "borrado con exito" });
+});
+servidor.patch("/edit", async (consulta, respuesta) => {
+  var data = consulta.body;
+  let nota = data.nota;
+  await baseDeDatos
+    .collection("notas")
+    .updateOne({ titulo: data.titulo }, { $set: { nota } });
+  respuesta.json({ res: "editado con exito" });
+});
--- publico/script/javasScript.js
+++ publico/script/javasScript.js
@@ -1,41 +1,69 @@
-async function guardar(){
-    var titulo = prompt("ingrese el titulo")
-    var nota = prompt("ingrese la nota")
-    var datos = await fetch('/guardar',{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({titulo:titulo, nota:nota})
-    })
-    mostrar()
+mostrar();
+async function guardar() {
+  var titulo = prompt("ingrese el titulo");
+  var nota = prompt("ingrese la nota");
+  var datos = await fetch("/guardar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ titulo: titulo, nota: nota })
+  });
+  agregar(titulo, nota);
 }
-async function borrar(){
-    var titulo = ""
-    var datos = await fetch('/delete',{
-        method:'DELETE',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({titulo:titulo})
-    })
-    mostrar()
+async function borrar(titulo) {
+  var titulo = titulo;
+  var datos = await fetch("/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ titulo: titulo })
+  });
+  var lista = document.querySelector(".lista");
+  lista.removeChild(document.getElementById(titulo));
 }
-function editar(){
-    var titulo = ""
-    var nota = prompt
-    var datos = await fetch('/edit',{
-        method:"PATCH",
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({titulo:titulo, nota:nota})
-    })
-    mostrar()
+async function editar(titulo) {
+  var titulo = titulo;
+  var notes = document.getElementById(titulo).firstElementChild
+    .lastElementChild;
+  var nota = prompt("edite su nota", notes.innerText);
+  var datos = await fetch("/edit", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ titulo: titulo, nota: nota })
+  });
+  notes.innerText = nota;
 }
-function mostrar(){
-    var respuesta = await fetch('/getNotas')
-    var contenido = await respuesta.json()
-
-
+async function mostrar() {
+  var respuesta = await fetch("/getNotas");
+  var contenido = await respuesta.json();
+  var lista = document.querySelector(".lista");
+  for (var i = 0; i < contenido.length; i++) {
+    lista.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div id="${contenido[i].titulo}">
+            <span><h3>${contenido[i].titulo}</h3><label>${contenido[i].nota}</label></span>
+            <button onclick="borrar('${contenido[i].titulo}')">borrar</button>
+            <button onclick="editar('${contenido[i].titulo}')">editar</button>
+        </div>
+        `
+    );
+  }
+}
+function agregar(titulo, nota) {
+  var lista = document.querySelector(".lista");
+  lista.insertAdjacentHTML(
+    "beforeend",
+    `
+          <div id="${titulo}">
+              <span><h3>${titulo}</h3><label>${nota}</label></span>
+              <button onclick="borrar('${titulo}')">borrar</button>
+              <button onclick="editar('${titulo}')">editar</button>
+          </div>
+          `
+  );
 }
